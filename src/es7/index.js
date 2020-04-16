946c55d843548096732c4341cc68bac89e330aa1
Daniel Vergara <danielvsuarez01@gmail.com>
2020-04-15 23:11:14 -0500

    cambios es7

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+/**ES7 */
+/**nuevo
+ * valor de includes
+ * operaciones exponencial
+ */
+
+let numbers = [1, 2, 3, 7, 8];
+if (numbers.includes(9)) {
+  console.log("Si se encuentra el valor");
+} else {
+  console.log("No se encuentra");
+}
+
+let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+
+console.log(result);
