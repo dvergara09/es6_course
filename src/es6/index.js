5254696bf4bd80e17bcfabed7650d346de8552ee
Daniel Vergara <danielvsuarez01@gmail.com>
2020-04-15 23:04:37 -0500

    clase 4

--- src/es6/index.js
+++ src/es6/index.js
@@ -125,3 +125,34 @@ const helloPromise = () => {
 helloPromise()
   .then((response) => console.log(response))
   .catch((error) => console.log(error));
+
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import hello from "./module";
+hello();
+
+function* helloworld() {
+  if (true) {
+    yield "hello, ";
+  }
+  if (true) {
+    yield "world";
+  }
+}
+const generatorHello = helloworld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
